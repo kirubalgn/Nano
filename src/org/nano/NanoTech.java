763226e2e2537cd2434085673a279cd24dfadef1
kirubalgn <kirubalgn@gmail.com>
2021-02-17 16:01:18 +0530

    module finished

--- src/org/nano/NanoTech.java
+++ src/org/nano/NanoTech.java
@@ -4,13 +4,9 @@ public class NanoTech {
 	
 	public void ring() {
 		System.out.println("Fire on");
-<<<<<<< HEAD
-		System.out.println("123");
-=======
 		System.out.println("Initialized");
-		
->>>>>>> 14a3671f775483068eb21e318868176f27ea00da
-	}
+		System.out.println("123");
+		}
 
 	public static void main(String[] args) {
 		
