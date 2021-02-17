300d06d3077ad8b6b2f2e2d3cac0513a2cfb9024
kirubalgn <kirubalgn@gmail.com>
2021-02-17 15:18:53 +0530

    created sucessfully

---
+++ src/org/nano/NanoTech.java
@@ -0,0 +1,18 @@
+package org.nano;
+
+public class NanoTech {
+	
+	public void ring() {
+		System.out.println("Fire on");
+
+	}
+
+	public static void main(String[] args) {
+		
+		NanoTech n= new NanoTech();
+		n.ring();
+				
+
+	}
+
+}
